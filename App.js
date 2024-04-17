b895b11367e394e4d325f64bc3abd09c966ea2dd
Jaswanth <139313622+Jaswanth0410@users.noreply.github.com>
2024-04-17 19:34:27 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -4,13 +4,29 @@ import './App.css';
 import Navbar from './Navbar';
 import Hero from './Hero'
 import Card from './card'
+import data from './data'
 
 export default function App() {
+
+  const cards = data.map(item => {
+    return(
+    <Card 
+      img={item.coverImg}
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+      location={item.location}
+      title={item.title}
+      price={item.price}
+      />
+      )
+  })
+    
+  
   return(
     <div>
       <Navbar />
       <Hero />
-      <Card />
+      {cards}
     </div>
   )
 }
--- card.js
+++ card.js
@@ -1,14 +1,22 @@
 import React from "react";
 import img1 from './image 12.jpg'
-import star from './Group 65.png'
+import star from './Star 1.png'
 
-export default function Card(){
+export default function Card(props){
+    console.log(props)
     return (
         <div className="card">
-            <img src={img1} alt="img1" className="card-img"/>
+            <img src={img1`${props.img}`} alt="img1" className="card-img"/>
             <div className="card-stats">
-                <img src={star} alt="stats" />
+                <img src={star} alt="stats" className="card-star"/>
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewcount})</span>
+                <span className="gray">{props.country}</span>
             </div>
+            <p>{props.title}</p>
+            <p><span className="bold">From $136</span>/ person</p>
         </div>
+
+
     )
-}
+}   
